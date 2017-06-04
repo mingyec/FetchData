d985a41c5191fd4823ea9b004512ad907adb2937
mingyec <mingyec@163.com>
2017-06-04 21:27:06 +0800

    添加时间处理模块和定时模块

--- DB/forecast_meteorological.sql
+++ DB/forecast_meteorological.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-06-03 11:11:03
+Date: 2017-06-04 21:25:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,8 +20,30 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `forecast_meteorological`;
 CREATE TABLE `forecast_meteorological` (
-  `site` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '地区',
-  `fetch_time` datetime DEFAULT NULL COMMENT '抓取时间',
+  `local` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '地区',
   `forecast_day` datetime DEFAULT NULL COMMENT '预测时间',
-  `forecast_temp` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '预测温度'
+  `temp_00` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '预测温度00:00',
+  `temp_01` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '预测温度01:00',
+  `temp_02` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '预测温度02:00',
+  `temp_03` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '预测温度03:00',
+  `temp_04` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '预测温度04:00',
+  `temp_05` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_06` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_07` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_08` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_09` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_10` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_11` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_12` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_13` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_14` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_15` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_16` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_17` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_18` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_19` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_20` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_21` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_22` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  `temp_23` varchar(255) COLLATE utf8_unicode_520_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
--- data/catch.js
+++ data/catch.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var mysql = require('mysql');
+var moment = require('moment');
+var schedule = require('node-schedule');
 
 //连接mysql测试
 var config = {
@@ -27,15 +29,10 @@ connection.on('error', function(err) {
     connection.connect();
 })
 
-var url = 'http://m.weathercn.com/zh/cn/panyu-district/2332592/current-weather/2332592?lang=zh-cn?partner=huawei13';
-// if (fetch_num === 0) {
-//     fetchData(url);
-// } else {
-//     setInterval(function() {
-//         fetchData(url);
-//     }, 5 * 60 * 1000);
-// }
+var url = 'http://m.weathercn.com/current-weather.do?partner=&id=2332592&language=zh-cn';
+var f_url = 'http://m.weathercn.com/hourly-weather-forecast.do?partner=&language=zh-cn&id=2332592';
 
+//抓取当前数据
 function fetchData(url) {
     http.get(url, function(res) {
         var html = '';
@@ -45,31 +42,63 @@ function fetchData(url) {
         res.on('end', function() {
             var $ = cheerio.load(html);
 
-            //时间格式化
-            var DateTime = $('#air>i>div:last-child').text();
-            var date = DateTime.split(' ');
-            var strDate = new Date().getFullYear() + '年' + date[0];
-            var fmDate = strDate.replace(/(\d{4}).(\d{1,2}).(\d{1,2}).+/mg, '$1-$2-$3');
-            var fetchTime = fmDate + ' ' + date[1];
+            try {
+                //时间格式化
+                var DateTime = $('#air>i>div:last-child').text();
+                var date = DateTime.split(' ');
+                var strDate = new Date().getFullYear() + '年' + date[0];
+                var fmDate = strDate.replace(/(\d{4}).(\d{1,2}).(\d{1,2}).+/mg, '$1-$2-$3');
+                var fetchTime = fmDate + ' ' + date[1];
 
-            //取得露点
-            var dewP = $('#day-part').children('p');
-            let dew = dewP[2].childNodes[1].data;
-            //地区、温度、湿度
+                //取得露点
+                var dewP = $('#day-part').children('p');
+                let dew = dewP[2].childNodes[1].data;
+                //地区、温度、湿度
+                let local = $('#current>h2>a').text();
+                let temp = $('#current>div:first-child>a>b').text();
+                let hum = $('.hum>p').text();
+                //风速，风向
+                let windP = $('.wind>p>span').children();
+                let wind = windP[1].prev.data.trim();
+                let windArr = wind.split(' ');
+                let wind_speed = windArr[1] + windArr[2];
+                let wind_direc = windArr[0];
+                //体感温度
+                let realFeel = $('.rfeel>b').text();
+
+                let meteoData = [fetchTime, local, temp, hum, dew, wind_speed, wind_direc, realFeel];
+                // insertSql(meteoData);
+            } catch (error) {
+                console.error(error);
+            }
+
+        })
+    })
+}
+
+function forecastData(f_url) {
+    http.get(f_url, function(res) {
+        var html = '';
+        res.on('data', function(chunk) {
+            html += chunk;
+        });
+        res.on('end', function() {
+            var $ = cheerio.load(html);
             let local = $('#current>h2>a').text();
-            let temp = $('#current>div:first-child>a>b').text();
-            let hum = $('.hum>p').text();
-            //风速，风向
-            let windP = $('.wind>p>span').children();
-            let wind = windP[1].prev.data.trim();
-            let windArr = wind.split(' ');
-            let wind_speed = windArr[1] + windArr[2];
-            let wind_direc = windArr[0];
-            //体感温度
-            let realFeel = $('.rfeel>b').text();
-
-            let meteoData = [fetchTime, local, temp, hum, dew, wind_speed, wind_direc, realFeel];
-            insertSql(meteoData);
+            let nowTime = moment().format('Y-MM-DD');
+            let tempArr = [local, nowTime],
+                sqlStr = '',
+                sqlVal = '';
+            for (let i = 0, max = 24; i < max; i++) {
+                let num = i < 10 ? '0' + i : i;
+                let ref = '#hour' + num;
+                let dd = $(ref).children('a').children('dl').children('dd');
+                let temp = dd.children('strong').text();
+                tempArr.push(temp);
+                sqlStr = sqlStr + ',temp_' + num;
+                sqlVal = i < 23 ? sqlVal + '?,' : sqlVal + '?';
+            }
+            insertFcDataToSql(tempArr, sqlStr, sqlVal);
         })
     })
 }
@@ -93,6 +122,10 @@ function loopCatch() {
 
 loopCatch();
 fetchData(url);
+/*schedule.scheduleJob('00 30 23 * * *', function() {
+    forecastData(f_url);
+})*/
+forecastData(f_url);
 
 function insertSql(data) {
     var insertStr = 'insert into real_meteorological(date_time,' +
@@ -106,4 +139,17 @@ function insertSql(data) {
                 // console.info(JSON.stringify(results));
         }
     });
+}
+
+function insertFcDataToSql(data, tempStr, val) {
+    let insertStr = 'insert into forecast_meteorological(local,forecast_day' +
+        tempStr + ') ' + 'values(' + val + ')';
+    console.log(tempStr.length, val.length);
+    connection.query(insertStr, data, function(err, results) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.info('success to insert forecast data to mysql');
+        }
+    })
 }
